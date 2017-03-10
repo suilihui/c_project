ff2a9ce1feec4439b139e992bc9f7a2e6ffbf036
suilihui <466066151@qq.com>
2017-03-10 22:15:16 +0800

    增加Calc

---
+++ Calc/include/head.h
@@ -0,0 +1,9 @@
+#ifndef _HEAD_H
+#define _HEAD_H
+
+int add(int, int);
+int sub(int, int);
+int mul(int, int);
+int div(int, int);
+
+#endif
---
+++ Calc/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "head.h"
+
+int main(void)
+{
+	int sum = add(2, 24);
+	printf("sum = %d\n", sum);
+}
